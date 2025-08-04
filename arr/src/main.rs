b99508a6228287fa7462d44076b8484cbbcdd5e7
SarahMK999 <sarahmimo999@gmail.com>
2025-08-04 12:23:12 +0300

    adding

---
+++ arr/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut arr = {1, 3, 5, 7};
+    for i in 0..arr.len{
+        println!("arr[{}]", i, arr[i]);
+    }
+}
---
+++ arr/target/debug/deps/arr.d
@@ -0,0 +1,5 @@
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\Bereshita1\arr\target\debug\deps\arr.d: src\main.rs
+
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\Bereshita1\arr\target\debug\deps\arr.exe: src\main.rs
+
+src\main.rs:
