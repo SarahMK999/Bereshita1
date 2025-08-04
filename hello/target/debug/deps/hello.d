93e2c43e813861a68c3c61b659376347a9c7a5d1
SarahMK999 <sarahmimo999@gmail.com>
2025-08-04 10:53:36 +0300

    second

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\Bereshita1\hello\target\debug\deps\hello.d: src\main.rs
+
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\Bereshita1\hello\target\debug\deps\hello.exe: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\sarah\OneDrive\שולחן\ העבודה\RUST\ PROGRAMING\Bereshita1\hello\target\debug\hello.exe: C:\Users\sarah\OneDrive\שולחן\ העבודה\RUST\ PROGRAMING\Bereshita1\hello\src\main.rs
