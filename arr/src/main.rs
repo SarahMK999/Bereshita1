58e4caf6384d0731b2ce9cc42cad3381626449d2
SarahMK999 <sarahmimo999@gmail.com>
2025-08-04 12:35:29 +0300

    updt

--- arr/src/main.rs
+++ arr/src/main.rs
@@ -1,6 +1,36 @@
 fn main() {
-    let mut arr = {1, 3, 5, 7};
-    for i in 0..arr.len{
-        println!("arr[{}]", i, arr[i]);
+    // let arr = [1, 3, 5, 7];
+    // for i in 0..arr.len(){
+    //     println!("arr[{}]: {}", i, arr[i]);
+    // }
+
+
+    // let arr1 = [2,4,6,8,10];
+    // let mut sum = 0;
+    // for num in arr1{
+    //     sum = sum + num;
+
+    // }
+    // println!("{}", sum);
+
+// let arr = [1,3,5,7,9,4,8,33,22];
+// let mut max = arr[0];
+// for i in 0..arr.len(){
+//     if arr[i]>max{
+//         max = arr[i];
+    
+//     }
+// }
+// println!("{}", max);
+
+
+let arr = [1,3,5,7,9,4,8,33,22];
+for num in arr{
+    if num%2 ==0{
+        println!("{}", num);
     }
 }
+
+}
+
+
---
+++ arr/target/debug/arr.d
@@ -0,0 +1,1 @@
+C:\Users\sarah\OneDrive\שולחן\ העבודה\RUST\ PROGRAMING\Bereshita1\arr\target\debug\arr.exe: C:\Users\sarah\OneDrive\שולחן\ העבודה\RUST\ PROGRAMING\Bereshita1\arr\src\main.rs
